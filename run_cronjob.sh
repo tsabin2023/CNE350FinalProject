650197cf63a1c07b5614d84c6541bef80c96d485
tsabin2023 <148308137+tsabin2023@users.noreply.github.com>
2024-06-10 13:21:01 -0700

    Update run_cronjob.sh
    sources added

--- run_cronjob.sh
+++ run_cronjob.sh
@@ -1,5 +1,11 @@
 !/bin/bash
 
+# source https://www.baeldung.com/linux/bandwidth-usage-monitoring
+# source https://www.geeksforgeeks.org/find-command-in-linux-with-examples/#
+# source Brian Huang
+# code modified by Tyler Sabin
+
+
 script_path="~/CNE350FinalProject/CNE350FinalBASH.sh"
 
 cron_job="*/1 * * * * $script_path"
