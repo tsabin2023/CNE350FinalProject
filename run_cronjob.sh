5ee1fb4b341043b4bff695393f19d0095597bfd1
tsabin2023 <148308137+tsabin2023@users.noreply.github.com>
2024-06-12 16:24:19 -0700

    Update run_cronjob.sh
    Unnecessary code removed.

--- run_cronjob.sh
+++ run_cronjob.sh
@@ -10,16 +10,8 @@ script_path="~/CNE350FinalProject/CNE350FinalBASH.sh"
 
 cron_job="*/1 * * * * $script_path"
 
-file_pattern="bandwidth_usage_*"
-
-log_directory="/var/log/vnstat_logs"
-
 echo "$cron_job" | crontab -
 
 echo "cron job added:"
 
 echo "$cron_job"
-
-find /var/log/vnstat_logs -name "bandwidth_usage_*.csv" -type f -mtime 1 -exec rm {} \;
-
-echo "log files older than 366 days have been deleted."
